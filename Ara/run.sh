24fa5e06836504959319953b21efbc7c3a0936dd
Nitin Verma <nitin.matrix@gmail.com>
2013-01-30 06:41:05 -0500

    made script place independent

--- Ara/run.sh
+++ Ara/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+curr_pwd=${PWD}
+cd $(dirname $0)
+script_pwd=${PWD}
+
 java -cp $(echo $(find target/dependency -type f | sed 's/$/:/g')target/Ara-1.0-SNAPSHOT.jar | sed -e 's/ //g') makesense.ara.Ara $@
+
+cd $curr_pwd
 
